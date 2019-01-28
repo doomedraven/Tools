040a9c042e6ecbfff24603810f55784c325229af
doomedraven <doomedraven@users.noreply.github.com>
2019-01-28 14:38:51 +0100

    big update

--- Windows/disable_win7noise.bat
+++ Windows/disable_win7noise.bat
@@ -32,14 +32,80 @@ REM disable IExplorer Proxy
 reg add "HKEY_CURRENT_USER\Software\Microsoft\Windows\CurrentVersion\Internet Settings" /v ProxyEnable /t  REG_DWORD /d 00000000 /f
 REM disable netbios in TCP/IP
 wmic nicconfig where index=8 call SetTcpipNetbios 2
-REM disable netbios service
+REM disable netbios service0
 reg add "HKLM\SYSTEM\CurrentControlSet\services\Imhosts" /v Start /t REG_DWORD /d 4 /f
 REM disable LLMNR
-reg add "HKLM\Software\policies\Microsoft\Windows NT\DNSClient" /v "EnableMulticast" /t REG_DWORD /d "0" /f
+reg add "HKLM\Software\policies\Microsoft\Windows NT\DNSClient" /v "EnableMulticast" /t REG_DWORD /d 0 /f
 REMdisable SQM
 reg add "HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\FlexGo\FGNotify\Prechecks"  /v Sqm /t REG_DWORD /d 00000002 /f
 REM Disable cert check
 reg add "HKLM\SYSTEM\CurrentControlSet\Services\HTTP\Parameters\SslBindingInfo" /v DefaultSslCertCheckMode /t REG_DWORD /d 1 /f
-; disable ClickToRunSvc
-sc stop "ClickToRunSvc" 
-sc config "ClickToRunSvc" start= disabled 
+REM disable ClickToRunSvc
+sc stop "ClickToRunSvc"
+sc config "ClickToRunSvc" start= disabled
+
+REM dr.watson
+reg add "HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows NT\CurrentVersion\AeDebug\AUTO" /v DWM.exe /t REG_DWORD /d 0 /f
+reg add "HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows NT\CurrentVersion\AeDebug\AutoExclusionList" /v DWM.exe /t REG_DWORD /d 0 /f
+
+REM curtain
+reg add "HKEY_LOCAL_MACHINE\SOFTWARE\Policies\Microsoft\Windows\PowerShell\ModuleLogging\ModuleNames" /v * /t REG_SZ /d * /f /reg:64
+reg add "HKEY_LOCAL_MACHINE\SOFTWARE\Policies\Microsoft\Windows\PowerShell\ScriptBlockLogging" /v EnableScriptBlockLogging /t REG_DWORD /d 00000001 /f /reg:64
+reg add "HKEY_LOCAL_MACHINE\SOFTWARE\Policies\Microsoft\Windows\PowerShell\Transcription" /v EnableTranscripting /t REG_DWORD /d 00000001 /f /reg:64
+reg add "HKEY_LOCAL_MACHINE\SOFTWARE\Policies\Microsoft\Windows\PowerShell\Transcription" /v OutputDirectory /t REG_SZ /d C:\PSTranscipts /f /reg:64
+reg add "HKEY_LOCAL_MACHINE\SOFTWARE\Policies\Microsoft\Windows\PowerShell\Transcription" /v EnableInvocationHeader /t REG_DWORD /d 00000001 /f /reg:64
+
+REM https://superuser.com/questions/972501/how-to-stop-microsoft-from-gathering-telemetry-data-from-windows-7-8-and-8-1
+sc stop DiagTrack
+sc stop dmwappushservice
+sc delete DiagTrack
+sc delete dmwappushservice
+echo "" > C:\ProgramData\Microsoft\Diagnosis\ETLLogs\AutoLogger\AutoLogger-Diagtrack-Listener.etl
+reg add "HKEY_LOCAL_MACHINE\SOFTWARE\Policies\Microsoft\Windows\DataCollection" /v AllowTelemetry /t REG_DWORD /d 0 /f
+reg add "HKEY_LOCAL_MACHINE\SYSTEM\ControlSet001\Control\WMI\AutoLogger" /v AutoLogger-Diagtrack-Listener /t DWORD /d 0 /f
+
+REM schtasks
+schtasks.exe /Change /TN "\Microsoft\Office\Office Automatic Updates 2.0" /Disable /ru ""
+schtasks.exe /Change /TN "\Microsoft\Office\Office ClickToRun Service Monitor" /Disable /ru ""
+schtasks.exe /Change /TN "\Microsoft\Office\Office Feature Updates" /Disable /ru ""
+schtasks.exe /Change /TN "\Microsoft\Office\Office Feature Updates Logon" /Disable /ru ""
+schtasks.exe /Change /TN "\Microsoft\Office\OfficeTelemetryAgentFallBack2016" /Disable /ru ""
+schtasks.exe /Change /TN "\Microsoft\Office\OfficeTelemetryAgentLogOn2016" /Disable /ru ""
+
+schtasks.exe /Change /TN "\Microsoft\Windows\Application Experience\AitAgent" /Disable /ru ""
+schtasks.exe /Change /TN "\Microsoft\Windows\Application Experience\Microsoft Compatibility Appraiser" /Disable /ru ""
+schtasks.exe /Change /TN "\Microsoft\Windows\Application Experience\ProgramData\Updater" /Disable /ru ""
+schtasks.exe /Change /TN "\Microsoft\Windows\Customer Experience Improvement Program\KernelCeipTask" /Disable /ru ""
+schtasks.exe /Change /TN "\Microsoft\Windows\Customer Experience Improvement Program\UsbCeip" /Disable /ru ""
+schtasks.exe /Change /TN "\Microsoft\Windows\Customer Experience Improvement Program\Consolidator" /Disable /ru ""
+schtasks.exe /Change /TN "\Microsoft\Windows\Autochk\Proxy" /Disable /ru ""
+schtasks.exe /Change /TN "\Microsoft\Windows\DiskDiagnostic\Microsoft-Windows-DiskDiagnosticDataCollector" /Disable /ru ""
+schtasks.exe /Change /TN "\Microsoft\Windows\Windows Media Sharing\UpdateLibrary" /Disable /ru ""
+
+REM Uninstall telemetry updates
+wusa /uninstall /kb:3065988 /quiet /norestart
+wusa /uninstall /kb:3083325 /quiet /norestart
+wusa /uninstall /kb:3083324 /quiet /norestart
+wusa /uninstall /kb:2976978 /quiet /norestart
+wusa /uninstall /kb:3075853 /quiet /norestart
+wusa /uninstall /kb:3065987 /quiet /norestart
+wusa /uninstall /kb:3050265 /quiet /norestart
+wusa /uninstall /kb:3050267 /quiet /norestart
+wusa /uninstall /kb:3075851 /quiet /norestart
+wusa /uninstall /kb:2902907 /quiet /norestart
+wusa /uninstall /kb:3068708 /quiet /norestart
+wusa /uninstall /kb:3022345 /quiet /norestart
+wusa /uninstall /kb:2952664 /quiet /norestart
+wusa /uninstall /kb:2990214 /quiet /norestart
+wusa /uninstall /kb:3035583 /quiet /norestart
+wusa /uninstall /kb:971033 /quiet /norestart
+wusa /uninstall /kb:3021917 /quiet /norestart
+wusa /uninstall /kb:3044374 /quiet /norestart
+wusa /uninstall /kb:3046480 /quiet /norestart
+wusa /uninstall /kb:3075249 /quiet /norestart
+wusa /uninstall /kb:3080149 /quiet /norestart
+wusa /uninstall /kb:2977759 /quiet /norestart
+wusa /uninstall /kb:3083710 /quiet /norestart
+wusa /uninstall /kb:3083711 /quiet /norestart
+wusa /uninstall /kb:3112336 /quiet /norestart
+wusa /uninstall /kb:3123862 /quiet /norestart
