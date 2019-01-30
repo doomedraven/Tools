a2bf8acb4e2562658e019f9631a0031948688dcd
doomedraven <doomedraven@users.noreply.github.com>
2019-01-30 11:37:14 +0100

    Create choco.bat

---
+++ Windows/choco.bat
@@ -0,0 +1,4 @@
+@"%SystemRoot%\System32\WindowsPowerShell\v1.0\powershell.exe" -NoProfile -InputFormat None -ExecutionPolicy Bypass -Command "iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))" && SET "PATH=%PATH%;%ALLUSERSPROFILE%\chocolatey\bin"
+
+choco upgrade chocolatey
+choco install -y dotnetfx dotnet4.7.2 vcredist-all wixtoolset msxml4.sp3 msxml6.sp1
