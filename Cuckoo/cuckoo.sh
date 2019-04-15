95a41b89cc39208ad51cea2ca213ecd64962343d
doomedraven <doomedraven@users.noreply.github.com>
2019-04-15 08:43:17 +0200

    sqlalchemy-utils

--- Cuckoo/cuckoo.sh
+++ Cuckoo/cuckoo.sh
@@ -103,7 +103,7 @@ EOF
     systemctl enable mongodb.service
     systemctl restart mongodb.service
 
-    pip install sqlalchemy jinja2 markupsafe bottle django chardet pygal django-ratelimit rarfile jsbeautifier dpkt nose dnspython pytz requests python-magic geoip pillow java-random python-whois git+https://github.com/crackinglandia/pype32.git git+https://github.com/kbandla/pydeep.git flask flask-restful flask-sqlalchemy
+    pip install sqlalchemy sqlalchemy-utils jinja2 markupsafe bottle django chardet pygal django-ratelimit rarfile jsbeautifier dpkt nose dnspython pytz requests python-magic geoip pillow java-random python-whois git+https://github.com/crackinglandia/pype32.git git+https://github.com/kbandla/pydeep.git flask flask-restful flask-sqlalchemy
     apt-get install -y openjdk-11-jdk-headless
     apt-get install -y openjdk-8-jdk-headless
     pip install distorm3 openpyxl git+https://github.com/volatilityfoundation/volatility.git PyCrypto #git+https://github.com/buffer/pyv8
