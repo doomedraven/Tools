2f05d9a3c016295a972c839a477bc0451a95f857
doomedraven <doomedraven@users.noreply.github.com>
2019-07-17 11:42:27 +0200

    libraries

--- Cuckoo/cuckoo.sh
+++ Cuckoo/cuckoo.sh
@@ -170,7 +170,7 @@ function dependencies() {
     # from pip import __main__
     # if __name__ == '__main__':
     #     sys.exit(__main__._main())
-    pip install requests[security] pyOpenSSL pefile tldextract httpreplay imagehash oletools olefile capstone PyCrypto voluptuous sflock xmltodict -U
+    pip install requests[security] pyOpenSSL pefile tldextract httpreplay imagehash oletools olefile capstone PyCrypto voluptuous sflock xmltodict future python-dateutil -U
     # re2
     apt-get install libre2-dev -y
     pip install re2
