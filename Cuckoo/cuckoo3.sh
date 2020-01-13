bb885a8993391bf18004240cc50da883d38f870d
doomedraven <doomedraven@users.noreply.github.com>
2020-01-13 12:17:46 +0100

    Update cuckoo3.sh

--- Cuckoo/cuckoo3.sh
+++ Cuckoo/cuckoo3.sh
@@ -470,9 +470,8 @@ function dependencies() {
     pip3 install cython
     pip3 install https://github.com/andreasvc/pyre2/archive/master.zip
 
-    #thanks Jurriaan <3, till merge, use enzok repo
-    #v8py hangs a lot so till it won't be fixed don't install it
-    pip3 install git+https://github.com/enzok/peepdf.git
+    #thanks Jurriaan <3
+    pip3 install git+https://github.com/jbremer/peepdf.git
 
     pip3 install matplotlib==2.2.2 numpy==1.15.0 six==1.11.0 statistics==1.0.3.5 lief==0.9.0
 
