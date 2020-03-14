3997eafb61a933e6f796b465b3a7cd0390b69535
doomedraven <doomedraven@users.noreply.github.com>
2020-03-14 01:29:59 +0100

    Update cape2.sh

--- Sandbox/cape2.sh
+++ Sandbox/cape2.sh
@@ -253,7 +253,7 @@ function install_yara() {
     cd ..
     rm $yara_version
     git clone --recursive https://github.com/VirusTotal/yara-python
-    pip3 install ./yara-python || return
+    pip3 install ./yara-python
 }
 
 function install_mongo(){
