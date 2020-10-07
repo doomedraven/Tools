e71f6ae30b9ffce9dc12c32f6901e29bc9f14a54
doomedraven <doomedraven@users.noreply.github.com>
2020-10-07 11:52:45 +0200

    thanks @ClaudioWayne

--- Sandbox/cape2.sh
+++ Sandbox/cape2.sh
@@ -622,6 +622,8 @@ function dependencies() {
     pip3 install git+https://github.com/doomedraven/sflock.git git+https://github.com/doomedraven/socks5man.git "pyattck>=2.0.2" distorm3 openpyxl git+https://github.com/volatilityfoundation/volatility3 git+https://github.com/DissectMalware/XLMMacroDeobfuscator passlib pyzipper
     #config parsers
     pip3 install git+https://github.com/Defense-Cyber-Crime-Center/DC3-MWCP.git git+https://github.com/kevthehermit/RATDecoders.git
+    
+    pip3 install "greenlet==0.4.16"
     # re2
     apt install libre2-dev -y
     #re2 for py3
