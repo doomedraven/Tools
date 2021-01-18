74da5d8b54d5e5e53d86b5a7b1a30b7f0046cebc
doomedraven <doomedraven@users.noreply.github.com>
2021-01-18 08:42:15 +0100

    psql 13

--- Sandbox/cape2.sh
+++ Sandbox/cape2.sh
@@ -620,13 +620,13 @@ EOF
 }
 
 function install_postgresql() {
-    echo "[+] Installing PostgreSQL 12"
+    echo "[+] Installing PostgreSQL"
 
     wget --quiet -O - https://www.postgresql.org/media/keys/ACCC4CF8.asc | sudo apt-key add -
     echo "deb http://apt.postgresql.org/pub/repos/apt/ `lsb_release -cs`-pgdg main" | sudo tee /etc/apt/sources.list.d/pgdg.list
 
     sudo apt update -y
-    sudo apt -y install libpq-dev postgresql-12 postgresql-client-12
+    sudo apt -y install libpq-dev postgresql-13 postgresql-client-13
 
     pip3 install psycopg2
 }
