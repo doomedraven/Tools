3c2cfc075870e40cbaaa67e935a9672590fed537
Nik Mohamad Aizuddin b. Nik Azmi <nik-mohamad-aizuddin@yandex.com>
2021-03-03 15:29:41 +0800

    fix(Sandbox/cape2.sh): change `--group` to `-g`

--- Sandbox/cape2.sh
+++ Sandbox/cape2.sh
@@ -757,7 +757,7 @@ function dependencies() {
     if id "${USER}" &>/dev/null; then
         echo "user ${USER} already exist"
     else
-        useradd --system --group ${USER} -d /home/${USER}/ -m ${USER}
+        useradd --system -g ${USER} -d /home/${USER}/ -m ${USER}
     fi
     # ToDo add current user to ${USER} group
     groupadd pcap
