4d83f77b7a9000544c210a87b9b6a53b9452a406
doomedraven <doomedraven@users.noreply.github.com>
2021-05-08 16:37:31 +0200

    cache rebuild after libvirt libs symlink

--- Virtualization/kvm-qemu.sh
+++ Virtualization/kvm-qemu.sh
@@ -479,6 +479,7 @@ EOH
         if [[ -n "$libvirt_so_path" ]]; then
             # #ln -s /usr/lib64/libvirt-qemu.so /lib/x86_64-linux-gnu/libvirt-qemu.so.0
             for so_path in $(ls "${libvirt_so_path}"libvirt*.so.0); do ln -sf "$so_path" /lib/$(uname -m)-linux-gnu/$(basename "$so_path"); done
+            ldconfig
         else
             echo "${RED}[!] Problem to create symlink, unknown libvirt_so_path path${NC}"
             exit 1
