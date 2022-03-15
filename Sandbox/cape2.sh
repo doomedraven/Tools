32a91f518d8571337cec9e0e6ca965092558df2d
doomedraven <doomedraven@users.noreply.github.com>
2022-03-15 17:00:48 +0100

    Update cape2.sh

--- Sandbox/cape2.sh
+++ Sandbox/cape2.sh
@@ -645,6 +645,7 @@ WantedBy=multi-user.target
 EOF
     fi
     sudo mkdir -p /data/{config,}db
+    systemctl unmask mongodb.service
     systemctl enable mongodb.service
     systemctl restart mongodb.service
 
